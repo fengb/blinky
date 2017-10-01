3bc2d75c3eb3ac3a025d0fa6a5d06dbee81f9243
Benjamin Feng <contact@fengb.me>
2017-09-30 19:41:36 -0500

    Bubble up errors

--- hangly.go
+++ hangly.go
@@ -11,30 +11,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func alpmToy() {
+func alpmToy() error {
 	reader, err := os.Open("/etc/pacman.conf")
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	conf, err := alpm.ParseConfig(reader)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	handle, err := conf.CreateHandle()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	localDb, err := handle.LocalDb()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	syncDbs, err := handle.SyncDbs()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	for _, pkg := range localDb.PkgCache().Slice() {
@@ -43,10 +43,15 @@ func alpmToy() {
 			fmt.Printf("%s %s -> %s\n", pkg.Name(), pkg.Version(), newPkg.Version())
 		}
 	}
+
+	return nil
 }
 
 func main() {
-	alpmToy()
+	err := alpmToy()
+	if err != nil {
+		panic(err)
+	}
 	// http.HandleFunc("/", handler)
 	// http.ListenAndServe(":8080", nil)
 }
