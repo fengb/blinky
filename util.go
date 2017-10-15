d2f70277041930f70fc4b2f8873b5d4577f53556
Benjamin Feng <contact@fengb.me>
2017-10-14 21:07:15 -0500

    Extracted out CallImmediate

--- util.go
+++ util.go
@@ -22,6 +22,23 @@ func (d Debounced) IsActive() bool {
 	return d.timer != nil
 }
 
+func (d *Debounced) CallImmediate() {
+	if d.timer != nil {
+		d.timer.Stop()
+		d.timer = nil
+	}
+
+	d.fn()
+	for _, drain := range d.drains {
+		select {
+		case drain <- struct{}{}:
+			close(drain)
+		default:
+		}
+	}
+	d.drains = nil
+}
+
 func (d *Debounced) Call() {
 	if d.IsActive() {
 		d.timer.Stop()
@@ -32,16 +49,7 @@ func (d *Debounced) Call() {
 	d.timer = time.NewTimer(d.delay)
 	go func() {
 		<-d.timer.C
-		d.timer = nil
-		d.fn()
-		for _, drain := range d.drains {
-			select {
-			case drain <- struct{}{}:
-				close(drain)
-			default:
-			}
-		}
-		d.drains = nil
+		d.CallImmediate()
 	}()
 }
 
