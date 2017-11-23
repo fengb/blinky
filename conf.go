421ca9255fed628f7b772f03300e871420338d41
Benjamin Feng <contact@fengb.me>
2017-11-23 09:36:05 -0600

    Separate parsing and mutation

--- conf.go
+++ conf.go
@@ -15,90 +15,23 @@ type Clock interface {
 	Second() int
 }
 
+type ConfHttp struct {
+	Listen string
+	Index  *template.Template
+}
+
+type ConfRefresh struct {
+	Enabled bool
+	At      Clock
+}
+
 type Conf struct {
-	Http struct {
-		Listen string
-		Index  *template.Template
-	}
-
-	Refresh struct {
-		Enabled bool
-		At      Clock
-	}
-
-	Pac *Pac
+	Http    ConfHttp
+	Refresh ConfRefresh
+	Pac     *Pac
 
 	dir     string
 	watches []chan struct{}
-}
-
-func (c *Conf) Watch() <-chan struct{} {
-	watch := make(chan struct{}, 1)
-	c.watches = append(c.watches, watch)
-	return watch
-}
-
-func (c *Conf) Reload() error {
-	cfg, err := ini.Load(c.dir + "/blinky.conf")
-	if err != nil {
-		return err
-	}
-
-	err = c.parseHttp(cfg.Section("http"))
-	if err != nil {
-		return err
-	}
-
-	err = c.parseRefresh(cfg.Section("refresh"))
-	if err != nil {
-		return err
-	}
-
-	for _, watch := range c.watches {
-		watch <- struct{}{}
-	}
-
-	return nil
-}
-
-func (c *Conf) parseHttp(sec *ini.Section) error {
-	var err error
-	if sec.HasKey("listen") {
-		c.Http.Listen = sec.Key("listen").Value()
-	}
-
-	_, _, err = net.SplitHostPort(c.Http.Listen)
-	if err != nil {
-		return err
-	}
-
-	c.Http.Index, err = template.ParseFiles(c.dir + "/index.html.tmpl")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *Conf) parseRefresh(sec *ini.Section) error {
-	var err error
-	if sec.HasKey("enabled") {
-		c.Refresh.Enabled, err = sec.Key("enabled").Bool()
-		if err != nil {
-			return err
-		}
-	}
-
-	if !sec.HasKey("at") {
-		c.Refresh.At, err = time.Parse(hhmm, "02:30")
-	} else {
-		c.Refresh.At, err = sec.Key("at").TimeFormat(hhmm)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func NewConf(dir string) (*Conf, error) {
@@ -117,4 +50,75 @@ func NewConf(dir string) (*Conf, error) {
 	}
 
 	return &conf, nil
+}
+
+func (c *Conf) Watch() <-chan struct{} {
+	watch := make(chan struct{}, 1)
+	c.watches = append(c.watches, watch)
+	return watch
+}
+
+func (c *Conf) Reload() error {
+	cfg, err := ini.Load(c.dir + "/blinky.conf")
+	if err != nil {
+		return err
+	}
+
+	c.Http, err = parseHttp(cfg.Section("http"), c.dir)
+	if err != nil {
+		return err
+	}
+
+	c.Refresh, err = parseRefresh(cfg.Section("refresh"))
+	if err != nil {
+		return err
+	}
+
+	for _, watch := range c.watches {
+		watch <- struct{}{}
+	}
+
+	return nil
+}
+
+func parseHttp(sec *ini.Section, dir string) (ConfHttp, error) {
+	c := ConfHttp{}
+	var err error
+	if sec.HasKey("listen") {
+		c.Listen = sec.Key("listen").Value()
+	}
+
+	_, _, err = net.SplitHostPort(c.Listen)
+	if err != nil {
+		return c, err
+	}
+
+	c.Index, err = template.ParseFiles(dir + "/index.html.tmpl")
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
+
+func parseRefresh(sec *ini.Section) (ConfRefresh, error) {
+	c := ConfRefresh{}
+	var err error
+	if sec.HasKey("enabled") {
+		c.Enabled, err = sec.Key("enabled").Bool()
+		if err != nil {
+			return c, err
+		}
+	}
+
+	if !sec.HasKey("at") {
+		c.At, err = time.Parse(hhmm, "02:30")
+	} else {
+		c.At, err = sec.Key("at").TimeFormat(hhmm)
+		if err != nil {
+			return c, err
+		}
+	}
+
+	return c, nil
 }
