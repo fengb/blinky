fd7b5c3cfbe42e83fbef911615d22654b8f72ab4
Benjamin Feng <contact@fengb.me>
2017-11-22 20:19:21 -0600

    Restart the http

--- http.go
+++ http.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func Serve(conf *Conf) error {
+func Serve(conf *Conf) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" || r.URL.Path != "/" {
 			http.NotFound(w, r)
@@ -24,7 +24,30 @@ func Serve(conf *Conf) error {
 		}
 	})
 
-	log.Println("Listening on", conf.Http.Listen)
-	http.ListenAndServe(conf.Http.Listen, nil)
-	return nil
+	confWatch := conf.Watch()
+
+	listenServer := func(addr string) http.Server {
+		srv := http.Server{Addr: addr}
+		go func() {
+			log.Println("Listening on", conf.Http.Listen)
+			err := srv.ListenAndServe()
+			if err != nil {
+				panic(err)
+			}
+		}()
+		return srv
+	}
+
+	srv := listenServer(conf.Http.Listen)
+	for _ = range confWatch {
+		if conf.Http.Listen != srv.Addr {
+			// TODO: why doesn't this stop listening?
+			err := srv.Shutdown(nil)
+			if err != nil {
+				panic(err)
+			}
+
+			srv = listenServer(conf.Http.Listen)
+		}
+	}
 }
