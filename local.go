b38b10d9035610e587b5c016211c4c01043c54a0
Benjamin Feng <contact@fengb.me>
2018-10-25 10:58:00 -0500

    Pushed pacman work into separate file

--- local.go
+++ local.go
@@ -1,146 +1,31 @@
 package main
 
-import (
-	"github.com/fsnotify/fsnotify"
-	"github.com/serverhorror/rog-go/reverse"
-	"log"
-	"os"
-	"strings"
-	"time"
-)
-
 type Local struct {
 	Snapshot *Snapshot
-	dbpath   string
-	logfile  string
-	watcher  *fsnotify.Watcher
 }
 
 func NewLocal() (*Local, error) {
-	loc := Local{}
-	var err error
-
-	if loc.dbpath == "" {
-		loc.dbpath = "/var/lib/pacman"
-	}
-
-	if loc.logfile == "" {
-		loc.logfile = "/var/log/pacman.log"
-	}
-
-	loc.Snapshot, err = loc.fetchSnapshot()
+	worker, err := NewLocalPacman()
 	if err != nil {
 		return nil, err
 	}
 
-	loc.watcher, err = fsnotify.NewWatcher()
+	snapshot, err := worker.FetchSnapshot()
 	if err != nil {
 		return nil, err
 	}
 
-	err = loc.watcher.Add(loc.dbpath)
-	if err != nil {
-		loc.watcher.Close()
-		return nil, err
-	}
+	loc := Local{Snapshot: snapshot}
+	go func() {
+		for snapshot := range worker.C {
+			loc.Snapshot = snapshot
+		}
+	}()
 
-	go loc.watchChanges()
-	go loc.watchErrors()
-
-	return &loc, nil
+	return &loc, err
 }
 
 func (l *Local) UpdateConf(conf *Conf) error {
-	// TODO: reload pacman.conf / watcher
+	// TODO: reload worker
 	return nil
-}
-
-func (l *Local) fetchSnapshot() (*Snapshot, error) {
-	snapshot := Snapshot{}
-
-	err := Parallel(
-		func() (err error) {
-			snapshot.LastSync, err = l.fetchLastSync()
-			return err
-		},
-		func() (err error) {
-			snapshot.Packages, err = l.fetchPackages()
-			return err
-		},
-	)
-
-	if err != nil {
-		return nil, err
-	}
-	return &snapshot, nil
-}
-
-func (l *Local) fetchLastSync() (time.Time, error) {
-	f, err := os.Open(l.logfile)
-	if err != nil {
-		return time.Time{}, err
-	}
-	defer f.Close()
-
-	var line string
-	scanner := reverse.NewScanner(f)
-	for scanner.Scan() {
-		line = scanner.Text()
-		if strings.Contains(line, "synchronizing") {
-			break
-		}
-	}
-
-	err = scanner.Err()
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	loc := time.Now().Location()
-	return time.ParseInLocation("2006-01-02 15:04", line[1:17], loc)
-}
-
-func (l *Local) fetchPackages() ([]Package, error) {
-	stdout, _, err := CmdRun("pacman", "-Qu")
-	if err != nil {
-		return nil, err
-	}
-
-	lines := strings.Split(stdout, "\n")
-	packages := make([]Package, len(lines))
-	for _, line := range lines {
-		tokens := strings.Split(line, " ")
-		pkg := Package{Name: tokens[0], Version: tokens[1], Upgrade: tokens[3]}
-		packages = append(packages, pkg)
-	}
-
-	return packages, nil
-}
-
-func (l *Local) watchChanges() {
-	debouncedUpdate := NewDebounced(1000, func() {
-		snapshot, err := l.fetchSnapshot()
-		if err != nil {
-			// How to handle?
-			log.Println(err)
-		} else {
-			l.Snapshot = snapshot
-		}
-	})
-
-	for event := range l.watcher.Events {
-		if strings.HasSuffix(event.Name, "db.lck") {
-			if event.Op&fsnotify.Create == fsnotify.Create {
-				debouncedUpdate.Cancel()
-			} else if event.Op&fsnotify.Remove == fsnotify.Remove {
-				debouncedUpdate.Call()
-			}
-		}
-	}
-}
-
-func (l *Local) watchErrors() {
-	for err := range l.watcher.Errors {
-		log.Println(err)
-	}
 }
---
+++ local_pacman.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/serverhorror/rog-go/reverse"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+var dbpath = "/var/lib/pacman"
+var logfile = "/var/log/pacman.log"
+
+type LocalPacman struct {
+	C chan *Snapshot
+
+	watcher *fsnotify.Watcher
+}
+
+func NewLocalPacman() (*LocalPacman, error) {
+	pac := LocalPacman{C: make(chan *Snapshot)}
+	var err error
+
+	pac.watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	err = pac.watcher.Add(dbpath)
+	if err != nil {
+		pac.watcher.Close()
+		return nil, err
+	}
+
+	go pac.watchChanges()
+	go pac.watchErrors()
+
+	return &pac, nil
+}
+
+func (p *LocalPacman) FetchSnapshot() (*Snapshot, error) {
+	snapshot := Snapshot{}
+
+	err := Parallel(
+		func() (err error) {
+			snapshot.LastSync, err = p.fetchLastSync()
+			return err
+		},
+		func() (err error) {
+			snapshot.Packages, err = p.fetchPackages()
+			return err
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &snapshot, nil
+}
+
+func (p *LocalPacman) fetchLastSync() (time.Time, error) {
+	f, err := os.Open(logfile)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer f.Close()
+
+	var line string
+	scanner := reverse.NewScanner(f)
+	for scanner.Scan() {
+		line = scanner.Text()
+		if strings.Contains(line, "synchronizing") {
+			break
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	loc := time.Now().Location()
+	return time.ParseInLocation("2006-01-02 15:04", line[1:17], loc)
+}
+
+func (p *LocalPacman) fetchPackages() ([]Package, error) {
+	stdout, _, err := CmdRun("pacman", "-Qu")
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(stdout, "\n")
+	packages := make([]Package, len(lines))
+	for _, line := range lines {
+		tokens := strings.Split(line, " ")
+		pkg := Package{Name: tokens[0], Version: tokens[1], Upgrade: tokens[3]}
+		packages = append(packages, pkg)
+	}
+
+	return packages, nil
+}
+
+func (p *LocalPacman) watchChanges() {
+	debouncedUpdate := NewDebounced(1000, func() {
+		snapshot, err := p.FetchSnapshot()
+		if err != nil {
+			// How to handle?
+			log.Println(err)
+		} else {
+			p.C <- snapshot
+		}
+	})
+
+	for event := range p.watcher.Events {
+		if strings.HasSuffix(event.Name, "db.lck") {
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				debouncedUpdate.Cancel()
+			} else if event.Op&fsnotify.Remove == fsnotify.Remove {
+				debouncedUpdate.Call()
+			}
+		}
+	}
+}
+
+func (p *LocalPacman) watchErrors() {
+	for err := range p.watcher.Errors {
+		log.Println(err)
+	}
+}
