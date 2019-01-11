71ee08079ed51c2f1edc94f0bbe564b7302b30cf
Benjamin Feng <contact@fengb.me>
2019-01-11 15:00:30 -0600

    Extract DailyTicker

--- refresh.go
+++ refresh.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Refresh struct {
-	conf  *Conf
-	timer *time.Timer
+	conf   *Conf
+	ticker *DailyTicker
 }
 
 func NewRefresh(conf *Conf) (Actor, error) {
-	r := Refresh{timer: time.NewTimer(1 * time.Hour)}
+	r := Refresh{ticker: NewDailyTicker(time.Now())}
 
 	err := r.UpdateConf(conf)
 	if err != nil {
@@ -21,11 +21,14 @@ func NewRefresh(conf *Conf) (Actor, error) {
 	}
 
 	go func() {
-		for {
-			<-r.timer.C
-			runRefresh()
-			r.scheduleNext()
+		for _ = range r.ticker.C {
+			log.Println("Running pacman --noconfirm -Syuwq")
+			_, _, err := CmdRun("pacman", "--noconfirm", "-Syuwq")
+			if err != nil {
+				log.Println(err)
+			}
 		}
+
 	}()
 
 	return &r, nil
@@ -37,49 +40,15 @@ func (r *Refresh) UpdateConf(conf *Conf) error {
 	}
 
 	r.conf = conf
-	r.scheduleNext()
+
+	r.ticker.Reset(conf.Refresh.At)
+	log.Println("Next refresh:", r.ticker.NextRun())
 
 	return nil
-}
-
-func (r *Refresh) scheduleNext() {
-	r.timer.Stop()
-	if r.conf.Refresh.Enable {
-		next := nextExecution(r.conf.Refresh.At)
-		log.Println("Next refresh:", next)
-		r.timer.Reset(time.Until(next))
-	}
 }
 
 func allowsPacmanSync() bool {
 	// Ideally use an exit status but all errors are 1
 	_, stderr, _ := CmdRun("pacman", "-S")
 	return !strings.Contains(stderr, "root")
-}
-
-func runRefresh() {
-	log.Println("Running pacman --noconfirm -Syuwq")
-	_, stderr, err := CmdRun("pacman", "--noconfirm", "-Syuwq")
-	if err != nil {
-		log.Println(stderr)
-	}
-}
-
-func nextExecution(target Clock) time.Time {
-	now := time.Now()
-	targetTime := time.Date(
-		now.Year(),
-		now.Month(),
-		now.Day(),
-		target.Hour(),
-		target.Minute(),
-		0,
-		0,
-		now.Location(),
-	)
-	if now.Before(targetTime) {
-		return targetTime
-	} else {
-		return targetTime.Add(24 * time.Hour)
-	}
 }
